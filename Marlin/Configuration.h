ce5503c9d4538561de76c3662f3cc12d2ccaaf85
rummsi <rummsi@gmail.com>
2022-11-13 11:03:54 +0000

    - Remoção de aviso na compilação do firmware.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3335,3 +3335,6 @@
 
 // Disable servo with M282 to reduce power consumption, noise, and heat when not in use
 //#define SERVO_DETACH_GCODE
+
+//###rummsi  - remoção de aviso na compilação do firmware
+#define DIAG_JUMPERS_REMOVED
