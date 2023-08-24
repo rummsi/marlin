d3a04e218020ebc2a63325e4b9692c08c610d402
rummsi <rummsi@gmail.com>
2023-08-24 23:19:02 +0100

    - Correção nos limites de impressão da mesa.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1698,7 +1698,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 225
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
